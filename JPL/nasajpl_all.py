9adc4a05f1a5d998b3b5dfb3a3792ebbaa322a5f
Behrouz <behrouz.safari@gmail.com>
2022-02-14 18:26:40 +0100

    download_jpl_elements

--- JPL/nasajpl_all.py
+++ JPL/nasajpl_all.py
@@ -32,51 +32,51 @@ def download_jpl_elements(target, t1, t2, step, center='500@10', ref_plane='ECLI
     text = text[mark1+6:]
     mark2 = text.find('$$EOE')
     text = text[:mark2]
-    rows = text.split('\n')[:-1]
-    n_columns = len(rows[0].split(',')[:-1])
+    raw_rows = text.split('\n')[:-1]
+    n_columns = len(raw_rows[0].split(',')[:-1])
+
+    cols = ['JDTDB', 'Calendar Date (TDB)', 'EC', 'QR', 'IN', 'OM', 'W', 'Tp', 'N', 'MA', 'TA', 'A', 'AD', 'PR']
+
+    raw_rows = [i.split(',')[:-1] for i in raw_rows]
+    rows = []
+    for r in raw_rows:
+        row = [i.strip() for i in r]
+        rows.append(row)
+
+    df = pd.DataFrame(data=rows, columns=cols)
     
-    times = [i.split(',')[1].strip()[5:] for i in rows]
-    times = [datetime.strptime(i, '%Y-%b-%d %H:%M:%S.%f') for i in times]
+    return df
 
-    OM = [i.split(',')[5].strip() for i in rows] # N
-    IN = [i.split(',')[4].strip() for i in rows] # i
-    W  = [i.split(',')[6].strip() for i in rows] # w
-    A  = [i.split(',')[11].strip() for i in rows]# a
-    EC = [i.split(',')[2].strip() for i in rows] # e
-    MA = [i.split(',')[9].strip() for i in rows] # M
+def modify_jpl_df(df):
+    df['Calendar Date (TDB)'] = \
+                 df['Calendar Date (TDB)'].str[5:]\
+                 .apply(lambda i: datetime.strptime(i, '%Y-%b-%d %H:%M:%S.%f'))
+    df = df.set_index('Calendar Date (TDB)')
+    df.index.name = 'TBD'
+    df = df.astype(float)
 
-    df = pd.DataFrame({'time': times, 'N':OM, 'i':IN, 'w':W, 'a':A, 'e':EC, 'M':MA})
-    df['time'] = pd.to_datetime(df['time'])
-    df[['N','i','w','a','e','M']] = df[['N','i','w','a','e','M']].astype(float)
+    # Calculate day (http://stjarnhimlen.se/comp/ppcomp.html)
+    t = pd.Series(df.index)
+    y = t.dt.year
+    m = t.dt.month
+    d = t.dt.day
+    H = t.dt.hour
+    M = t.dt.minute
+    S = t.dt.second
+    MS = t.dt.microsecond
+    day = 367*y - 7 * ( y + (m+9)//12 ) // 4 + 275*m//9 + d - 730530
+    UT = H + M/60 + S/3600 + MS/3600000000
+    df['day'] = list(day + UT)
+    return df
 
-    df['_y'] = df['time'].dt.year
-    df['_m'] = df['time'].dt.month
-    df['_d'] = df['time'].dt.day
-    df['_H'] = df['time'].dt.hour
-    df['_M'] = df['time'].dt.minute
-    df['_S'] = df['time'].dt.second
-    df['_MS'] = df['time'].dt.microsecond
-
-    df['day'] = 367*df['_y'] - 7 * ( df['_y'] + (df['_m']+9)//12 ) // 4 + 275*df['_m']//9 + df['_d'] - 730530
-    df['UT'] = df['_H'] + df['_M']/60 + df['_S']/3600 + df['_MS']/3600000000
-    df['day'] = df['day'] + df['UT']/24
-
-    df = df[['time', 'day', 'N', 'i', 'w', 'a', 'e', 'M']]
-    df = df.set_index('time')
-    
-    return df, text, rows, n_columns, times
-
+"""
 t1 = '2022-02-11T10:00:00'
 t2 = '2022-02-12T10:00:00'
 
-df, text, raw_rows, n_columns, times = download_jpl_elements(399, t1, t2, step=5)
+df = download_jpl_elements(399, t1, t2, step=5)
+df = modify_jpl_df(df)
 
-cols = ['JDTDB', 'Calendar Date (TDB)', 'EC', 'QR', 'IN', 'OM', 'W', 'Tp', 'N', 'MA', 'TA', 'A', 'AD', 'PR']
 
-raw_rows = [i.split(',')[:-1] for i in raw_rows]
-rows = []
-for r in raw_rows:
-    row = [i.strip() for i in r]
-    rows.append(row)
+# d = 367*y - 7 * ( y + (m+9)/12 ) / 4 + 275*m/9 + D - 730530
+"""
 
-df = pd.DataFrame(data=rows, columns=cols)
